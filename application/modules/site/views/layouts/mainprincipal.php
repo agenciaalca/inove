ec66b05ffefc657f8a4bf6da8a8a191baef7609e
cristiianefaria <cristiane.trc@gmail.com>
2017-12-21 14:27:52 -0200

    inserindo pagina em construção

--- application/modules/site/views/layouts/mainprincipal.php
+++ application/modules/site/views/layouts/mainprincipal.php
@@ -20,69 +20,29 @@
         <link rel="stylesheet" href="<?= base_url() ?>assets/css/jquery.bxslider.css">
         <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/css/estilo.css">
         <link rel="stylesheet" type="text/css" href="<?= base_url() ?>assets/css/style.css">
-        <style>
-            p{
-                font-size: 16px;
-            }
-            .logomarca{
-                display: block;
-                position: relative;
-                width: 100%;
-                margin: 0;
-                padding: 0;
-                word-wrap: break-word;
-            }
-
-            .logo{
-                background-image: url("./assets/images/bg.png") ;
-                background-repeat: repeat-x;
-                background-color: #1150a0;
-                display: block;
-                padding: 10px 0;
-                text-align: center;
-            }
-            .temporario{
-                background-image: url('./assets/img/banner-temporario.png');
-                
-                text-align: center;
-                width: 100%;
-            }
-            .temporario .container{
-                padding-top: 200px;
-                padding-bottom: 200px;
-            }
-            .temporario h1{
-                font-size: 40px;
-                font-weight: bold;
-                margin-bottom: 15px;
-                color:#fff;
-            }
-            .temporario p{
-                color:#fff;
-                font-size: 18px;
-                margin-bottom: 60px;
-            }
-            .temporario .redes-sociais a{
-                margin: 0 15px;
-            }
-            .divider{
-                margin: 20px 0;
-            }
-        </style>
     </head>
 
     <body>
-        <!-- Fixed logomarca -->
-        <div class="logomarca">
-            <a class="logo" href="http://inovegondolas.com.br/"><img src="assets/img/logo.png" height="60px" align="center"></a>
-        </div>
+        <nav class="navbar navbar-default">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    
+                    <a class="navbar-brand" href="<?= base_url() ?>">
+                        <img src="<?= base_url() ?>assets/images/logo-inove.png" width="90px">
+                    </a>
+                </div>
+
+                
+            </div>
+        </nav>
 
         <?= $content ?>
 
         <footer id="footer" class="bgi-gray">
             <div class="container">
-                <div class="row">
-                    <div class="footer-page">
+               <div class="row">
+                     <div class="footer-page">
                         <div class="titulos-de-contexto ">
                             <h1 class="block_title">CONTATO</h1>
                             <p>Entre em contato consoco e tire suas dúvidas!</p>
@@ -118,32 +78,31 @@
                                 </div>
                             </form>
                         </div>
-                        <div class="divider hidden-md hidden-lg"></div>
                         <div class="col-md-4">
                             <div class="informacoes-contato">
                                 <p><i class="fa fa-envelope" aria-hidden="true"></i> contato@inovegondolas.com.br</p>
-                                <p><i class="fa fa-whatsapp" aria-hidden="true"></i> (62) 3581-1117</p>
-                                <p><i class="fa fa-globe" aria-hidden="true"></i> Rua 52, Qd 3, Lt 8, Santos Dumont, Goiânia</p>
+                                <p><i class="fa fa-whatsapp" aria-hidden="true"></i> (062) 3636-0111</p>
+                                <p><i class="fa fa-globe" aria-hidden="true"></i> Rua 88-B, nº 73, Setor Sul, Goiânia - GO, 74085-030</p>
                             </div>
-                            <!--                            <div class="cartoes">
-                                                            <div class="cartoes-c col-xs-4">
-                                                                <img class="img-responsive" src="<?= base_url() ?>assets/img/icons-11.png">
-                                                            </div>
-                                                            <div class="cartoes-c col-xs-4">
-                                                                <img class="img-responsive"  src="<?= base_url() ?>assets/img/icons-10.png">
-                                                            </div>
-                                                            <div class="cartoes-c col-xs-4">
-                                                                <img class="img-responsive" src="<?= base_url() ?>assets/img/icons-08.png">
-                                                            </div>
-                                                        </div>-->
+                            <div class="cartoes">
+                                <div class="cartoes-c col-xs-4">
+                                    <img class="img-responsive" src="<?= base_url() ?>assets/img/icons-11.png">
+                                </div>
+                                <div class="cartoes-c col-xs-4">
+                                    <img class="img-responsive"  src="<?= base_url() ?>assets/img/icons-10.png">
+                                </div>
+                                <div class="cartoes-c col-xs-4">
+                                    <img class="img-responsive" src="<?= base_url() ?>assets/img/icons-08.png">
+                                </div>
+                            </div>
                         </div>
                         <div class="col-xs-12">
-                            <!--                            <div class="redes-sociais">
-                                                            <a href="#" target="_blank"><img src="<?= base_url() ?>assets/images/fb.png"></a>
-                                                            <a href="#" target="_blank"><img src="<?= base_url() ?>assets/images/ig.png"></a>
-                                                            <a href="#" target="_blank"><img src="<?= base_url() ?>assets/images/in.png"></a>
-                                                        </div>-->
-                            <div class="divider hidden-md hidden-lg"></div>
+                            <div class="redes-sociais">
+                                <a href="#" target="_blank"><img src="<?= base_url() ?>assets/images/fb.png"></a>
+                                <a href="#" target="_blank"><img src="<?= base_url() ?>assets/images/ig.png"></a>
+                                <a href="#" target="_blank"><img src="<?= base_url() ?>assets/images/in.png"></a>
+                            </div>
+
                             <div class="copyright">© INOVE GONDOLAS. ALL RIGHTS RESERVED. DESENVOLVIDO POR <a href="http://agenciaalca.com" target="_blank"> AGÊNCIA ALCA</a></div>        
                         </div>
                     </div>
--- application/modules/site/views/pages/home.php
+++ application/modules/site/views/pages/home.php
@@ -1,91 +1,11 @@
-<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
 
-    <div class="carousel-inner" role="listbox">
-        <div class="item active">
-            <img class="img-responsive" src="<?= base_url() ?>assets/images/fundo.png" alt="...">
-            <div class="carousel-caption">
-            </div>
-        </div>
-        <div class="item">
-            <img class="img-responsive" src="<?= base_url() ?>assets/images/fundo.png" alt="...">
-            <div class="carousel-caption">
-            </div>
-        </div>
-        <div class="item">
-            <img class="img-responsive" src="<?= base_url() ?>assets/images/fundo.png" alt="...">
-            <div class="carousel-caption">
-            </div>
-        </div>
-    </div>
-
-    <a class="left carousel-control scrooldown" href="#carousel-example-generic" role="button" data-slide="prev">
-        <span>
-            <img src="<?= base_url() ?>assets/images/seta_esq.png" />
-        </span>
-    </a>
-    <a class="right carousel-control scrooldown" href="#carousel-example-generic" role="button" data-slide="next">
-        <span>
-            <img src="<?= base_url() ?>assets/images/seta_dire.png" />
-        </span>
-    </a>
-</div>
-
-<div class="container top20 bot20">
-    <div class="row">
-        <div class="col-md-6 bot20">
-            <div class="titulo linha">Seja bem vindo!</div>
-            <?= $about ?>
-            <div class="titulo linha">Seja bem vindo!</div>
-            <?= $about ?>
-            <div class="titulo linha">Seja bem vindo!</div>
-            <p>Conteudo do sdjfsadkfjk</p>
-
-        </div>
-
-        <div class="col-md-6">
-            <div class="titulo linha">Conheça nossas linhas</div>
-            <div class="nossas-linhas">
-                <div class="aesquerda">
-                    <div class="over1">
-                        <p>Armazenagem</p>
-                        <div class="absolute"></div>
-                    </div>
-                    <div class="over2">
-                        <p>Casa</p>
-                        <div class="absolute"></div>
-                    </div>
-                    <div class="over3">
-                        <p>Farmácia</p>
-                        <div class="absolute"></div>
-                    </div>
-                    <div class="over4">
-                        <p>Mercado</p>
-                        <div class="absolute"></div>
-                    </div>
-                    <div class="over5">
-                        <p>Festa</p>
-                        <div class="absolute"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <hr>
-    </div>
-</div>
-
-<div class="container" style="margin-bottom: 30px;">
-    <div class="row">
-        <div class="col-xs-12">
-            <div class="titulo linha">Clientes</div>
-            <div class="brand-slider">
-                <div id="owl-brand" class="owl-carousel">
-                    <?php foreach ($customers as $customer): ?>
-                        <div class="item">
-                            <a  href="<?= base_url('clientes') ?>"><img src="<?= base_url('assets/img/customers/' . $customer->logo) ?>" style="width: 190px; height: 140px;" alt=""></a>
-                        </div>
-                    <?php endforeach ?>
-                </div>
-            </div>
+<div class="temporario">
+    <div class="container">
+        <h1>EM CONSTRUÇÃO</h1>
+        <P>ENQUANTO ISSO, ENTRE EM CONTATO COM NOSSA EQUIPE PARA MAIS INFORMAÇÕES</P>
+        <div class="redes-sociais">
+            <a href="https://facebook.com/INOVE-Gondolas-917545585059648" target="_blank"><img src="<?= base_url() ?>assets/images/fb.png"></a>
+            <a href="https://www.instagram.com/inovegondolas/" target="_blank"><img src="<?= base_url() ?>assets/images/ig.png"></a>
         </div>
     </div>
 </div>
